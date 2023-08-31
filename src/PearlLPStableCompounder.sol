1668c9ab977d5d6b8debb13ba5b528cca67966d1
spalen0 <spalen@proton.me>
2023-08-31 21:31:20 +0200

    chore: remove comment

--- src/PearlLPStableCompounder.sol
+++ src/PearlLPStableCompounder.sol
@@ -273,7 +273,6 @@ contract PearlLPStableCompounder is BaseHealthCheck, CustomStrategyTriggerBase {
     function reportTrigger(
         address /*_strategy*/
     ) external view override returns (bool, bytes memory) {
-        // @todo do we need to verify _strategy == address(this)? it cannot harm strategy if we don't check it
         if (TokenizedStrategy.isShutdown()) return (false, bytes("Shutdown"));
         // gas cost is not concern here
         if (balanceOfAsset() > 0 || balanceOfRewards() > minRewardsToSell) {
